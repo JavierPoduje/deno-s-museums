8ddec49b65ca651d40573deec0d89ecea8508e91
JavierPoduje <javier.poduje@gmail.com>
2022-09-29 21:10:55 -0300

    feat: more tests...

--- src/index.test.ts
+++ src/index.test.ts
@@ -46,23 +46,8 @@ Deno.test("it returns user and token when user logs in", async () => {
   const server = await createTestServer();
 
   // Registering a user
-  const { user: registeredUser } = await fetch(
-    "http://localhost:9001/api/users/register",
-    {
-      method: "POST",
-      headers: jsonHeaders,
-      body: JSON.stringify({ username: "asantos00", password: "abcd" }),
-    },
-  ).then((r) => r.json());
-
-  const response = await fetch("http://localhost:9001/api/login", {
-    method: "POST",
-    headers: jsonHeaders,
-    body: JSON.stringify({
-      username: registeredUser.username,
-      password: "abcd",
-    }),
-  }).then((r) => r.json());
+  await register("test-user", "test-password");
+  const response = await login("test-user", "test-password");
 
   t.assertEquals(response.user.username, "asantos00", "returns username");
   t.assert(!!response.user.createdAt, "has createdAt date");
@@ -70,3 +55,61 @@ Deno.test("it returns user and token when user logs in", async () => {
 
   server.controller.abort();
 });
+
+Deno.test("it should let users with a valid token access the museums list", async () => {
+  const jsonHeaders = new Headers();
+  jsonHeaders.set("content-type", "application/json");
+  const server = await createTestServer();
+  await register("test-user", "test-password");
+  const { token } = await login("test-user", "test-password");
+
+  const authenticateHeaders = new Headers();
+  authenticateHeaders.set("content-type", "application/json");
+  authenticateHeaders.set("authorization", `Bearer ${token}`);
+  const { museums } = await fetch("http://localhost:9001/api/museums", {
+    headers: authenticateHeaders,
+  }).then((r) => {
+    t.assertEquals(r.status, 200);
+    return r;
+  }).then((r) => r.json());
+
+  t.assertEquals(museums.length, 0);
+  server.controller.abort();
+});
+
+Deno.test("it should respond with a 401 to a user with an invalid token", async () => {
+  const server = await createTestServer();
+  const authenticateHeaders = new Headers();
+  authenticateHeaders.set("content-type", "application/json");
+  authenticateHeaders.set("authorization", `Bearer invalid-token`);
+  const response = await fetch("http://localhost:9001/api/museums", {
+    headers: authenticateHeaders,
+    body: JSON.stringify({ username: "test-user", password: "test-password" }),
+  });
+  t.assertEquals(response.status, 401);
+  t.assertEquals(await response.text(), "Authentication failed");
+  server.controller.abort();
+});
+
+function register(username: string, password: string) {
+  const jsonHeaders = new Headers();
+  jsonHeaders.set("content-type", "application/json");
+  return fetch(
+    "http://localhost:9001/api/users/register",
+    {
+      method: "POST",
+      headers: jsonHeaders,
+      body: JSON.stringify({ username, password }),
+    },
+  ).then((r) => r.json());
+}
+
+function login(username: string, password: string) {
+  const jsonHeaders = new Headers();
+  jsonHeaders.set("content-type", "application/json");
+  return fetch("http://localhost:9001/api/login", {
+    method: "POST",
+    headers: jsonHeaders,
+    body: JSON.stringify({ username, password }),
+  }).then((r) => r.json());
+}
